ca3ee9cc87395bd76a6b89529203e430033144db
Graham Gilpatric <grahamwithag@gmail.com>
2019-01-16 18:50:43 -0500

    basic chatbox component

--- src/client/components/ChatBox.js
+++ src/client/components/ChatBox.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const ChatBox = (props) => {
-  return (
-    <React.Fragment>
+class ChatBox extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      chat: []
+    }
+  }
 
-    </React.Fragment>
-  )
+  render() {
+    let messages = this.state.chat.map(message => {
+      return(
+        <li>{message.username}: {message.message}</li>
+      )
+    })
+
+    return (
+      <ul>
+        {messages}
+      </ul>
+    )
+  }
 }
 
 export default ChatBox;
