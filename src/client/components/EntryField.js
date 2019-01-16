6c0841b5af15ba6eb527e90856b13b35a869b438
Graham Gilpatric <grahamwithag@gmail.com>
2019-01-16 18:17:32 -0500

    switched entryfield to stateless component

--- src/client/components/EntryField.js
+++ src/client/components/EntryField.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class EntryField extends Component {
-  render() {
-    return (
-      <form onSubmit={this.props.send}>
-        <span>Username: </span>
-        <input
-          name='username'
-          type='text'
-          value={this.props.username}
-          onChange={this.props.handleChange}
-        /><br/>
-        <input
-          name='message'
-          type='text'
-          value={this.props.message}
-          onChange={this.props.handleChange}
-        />
-        <button>Send</button>
-      </form>
-    );
-  }
+const EntryField = (props) => {
+  return (
+    <form onSubmit={props.send}>
+      <label>Username: </label>
+      <input
+        name='username'
+        type='text'
+        value={props.username}
+        onChange={props.handleChange}
+      /><button type='button'>Change Name</button><br/>
+      <label>Message: </label>
+      <input
+        name='message'
+        type='text'
+        value={props.message}
+        onChange={props.handleChange}
+      />
+      <button>Send</button>
+    </form>
+  );
 }
 
 export default EntryField;
