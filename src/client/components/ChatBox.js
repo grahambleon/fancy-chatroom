dbe3ff88ac9c62735cd4f355c68d980c06218841
Graham Gilpatric <grahamwithag@gmail.com>
2019-01-16 19:36:52 -0500

    generates message ids

--- src/client/components/ChatBox.js
+++ src/client/components/ChatBox.js
@@ -1,26 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ChatBox extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      chat: []
-    }
-  }
-
-  render() {
-    let messages = this.state.chat.map(message => {
-      return(
-        <li>{message.username}: {message.message}</li>
-      )
-    })
-
-    return (
-      <ul>
-        {messages}
-      </ul>
+const ChatBox = (props) => {
+  let messages = props.chat.map(message => {
+    return(
+      <li key={message.id}>{message.username}: {message.message}</li>
     )
-  }
+  })
+
+  return (
+    <ul>
+      {messages}
+    </ul>
+  )
 }
 
 export default ChatBox;
