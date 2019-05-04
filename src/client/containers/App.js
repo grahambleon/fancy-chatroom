461440dbccf1344dc835d1d6514b9fc766ddc5f6
Graham Gilpatric <grahamwithag@gmail.com>
2019-05-03 23:25:47 -0400

    created chat component to move logic out of app

--- src/client/containers/App.js
+++ src/client/containers/App.js
@@ -1,78 +1,19 @@
-import React, { Component } from 'react'
-import io from 'socket.io-client'
-import EntryField from '../components/EntryField'
-import ChatBox from '../components/ChatBox'
+import React from 'react'
+import styled from 'styled-components'
+import Chat from './Chat.js'
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      endpoint: 'http://localhost:4001/',
-      chat: [],
-      username: 'Anonymous',
-      usernameField: 'Anonymous',
-      message: ''
-    }
-    this.changeUsername = this.changeUsername.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.send = this.send.bind(this)
-    this.socket = io(this.state.endpoint)
-  }
+const StyledChat = styled.div`
+  text-align: center;
+  border: thick black;
+  width: 50%;
+`
 
-  changeUsername() {
-    const payload = {
-      newName: this.state.usernameField,
-      oldName: this.state.username,
-      id: `${Date.now()}${this.state.username}`
-    }
-    this.socket.emit('namechange', payload)
-    this.setState({ username: this.state.usernameField })
-  }
-
-  componentDidMount() {
-    this.socket.on('message', payload => {
-      this.setState({ chat: this.state.chat.concat(
-        <li key={payload.id}>{payload.username}: {payload.message}</li>
-      )})
-    })
-
-    this.socket.on('namechange', payload => {
-      this.setState({ chat: this.state.chat.concat(
-        <li key={payload.id}>{payload.oldName} has changed their name to {payload.newName}</li>
-      )})
-    })
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
-
-  send(event) {
-    event.preventDefault()
-
-    const payload = {
-      username: this.state.username,
-      message: this.state.message,
-      id: `${Date.now()}${this.state.username}`
-    }
-    this.socket.emit('message', payload)
-    this.setState({ message: '' })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <ChatBox chat={this.state.chat}/>
-        <EntryField
-          handleChange={this.handleChange}
-          send={this.send}
-          username={this.state.usernameField}
-          changeUsername={this.changeUsername}
-          message={this.state.message}
-        />
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  return(
+    <StyledChat>
+      <Chat />
+    </StyledChat>
+  )
 }
 
 export default App;
